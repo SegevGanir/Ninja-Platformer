e69c4b349249525fef3fca19446788034946d436
SegevGanir <Segev@ganir.net>
2023-11-17 16:49:23 -0800

    modified:   game.py 	new file:   scripts/__pycache__/entities.cpython-39.pyc 	new file:   scripts/__pycache__/utils.cpython-39.pyc 	new file:   scripts/entities.py 	new file:   scripts/utils.py

--- game.py
+++ game.py
@@ -2,60 +2,53 @@ import sys
 
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
 
 class Game:
+  def __init__(self):
+    pygame.init()
 
-    def __init__(self):
-        pygame.init()
+    icon = pygame.image.load('favicon.png')
+    pygame.display.set_icon(icon)
 
-        icon = pygame.image.load('favicon.png')
-        pygame.display.set_icon(icon)
+    pygame.display.set_caption('ninja game')
+    self.screen = pygame.display.set_mode((640, 480))
 
-        pygame.display.set_caption('ninja game')
-        self.screen = pygame.display.set_mode((640, 480))
+    self.clock = pygame.time.Clock()
+    
+    self.movement = [False, False]
 
-        self.clock = pygame.time.Clock()
+    self.assets = {
+      'player': load_image('entities/player.png')
+    }
+    
+    self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.img.set_colorkey((0, 0, 0)) 
+  def run(self):
+    while True:
+      self.screen.fill((14, 219, 249))
 
-        self.img_pos = [160, 260]
-        self.movement = [False, False]
- 
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+      self.player.update((self.movement[1] - self.movement[0], 0))
+      self.player.render(self.screen)
 
-    def run(self):
-        while True:
-            self.screen.fill((14, 219, 249))
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_LEFT:
+            self.movement[0] = True
+          if event.key == pygame.K_RIGHT:
+            self.movement[1] = True
+        if event.type == pygame.KEYUP:
+          if event.key == pygame.K_LEFT:
+            self.movement[0] = False
+          if event.key == pygame.K_RIGHT:
+            self.movement[1] = False
 
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 255), self.collision_area)
- 
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
-
-
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
-                        self.movement[1] = True
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
-                        self.movement[1] = False
-
-            pygame.display.update()
-            self.clock.tick(60)
+      pygame.display.update()
+      self.clock.tick(60)
 
 
 Game().run()
