46fa23280ddeef8c2840f3aedfec250b82bc66f7
SegevGanir <Segev@ganir.net>
2023-11-17 18:04:05 -0800

    54:40 modified:   scripts/tilemap.py modified:   scripts/utils.py

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -3,3 +3,7 @@ class Tilemap:
     self.tile_size = tile_size
     self.tilemap = {}
     self.offgrid_tiles = []
+
+    for i in range(10):
+      self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+      self.tilemap[';10' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, i + 5)}
